800434167be71983044c37074c367efb76821a42
unknown <todorovicslobodan10@gmail.com>
2022-07-16 11:07:27 +0100

    adding dummy data and rendering it afterwards

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assests/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       {/* class name is in square brackets because of - in class name */}
       <div className={classes["main-image"]}>
